31556351282f7e9af8a61cb3d6f0e779473e681a
OverlandJr <127855317+OverlandJr@users.noreply.github.com>
2023-10-07 09:30:19 -0300

    Update pilha.h

--- pilha.h
+++ pilha.h
@@ -1,11 +1,16 @@
 #ifndef PILHA_H
 #define PILHA_H
 
+// defini o tamanho, o topo da pilha (índice do elemento no topo).
+
 typedef struct {
     double *itens;
     int tamanho;
     int topo;
 } Pilha;
+
+
+// declara as funções incluindo a criação da pilha, empilhar, desempilhar, verificar o tamanho, verificar se está vazia e destruir a pilha.
 
 Pilha *criarPilha(int tamanho);
 void empilhar(Pilha *pilha, double valor);
